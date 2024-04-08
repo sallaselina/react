376e8743f0c9a6cd1b7eaa4cdca3fd97114bb5a7
Margit Tennosaar <margit.tennosaar@bc.fi>
2024-04-08 15:12:00 +0300

    added monday lecture practice

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import { useState } from 'react';
+import Header from './Header.jsx';
+import Footer from './Footer.jsx';
+import Form from './Form.jsx';
+import View from './View.jsx';
 
 function App() {
-  const [count, setCount] = useState(0)
+  /*   const [counter, setCounter] = useState(1000);
+  const [persons, setPersons] = useState([
+    { id: 1, name: 'Margit', title: 'CTO', location: 'Helsinki' },
+    { id: 2, name: 'Karin', title: 'designer', location: 'Tartu' },
+    { id: 3, name: 'Kati', title: 'developer', location: 'Far away' },
+  ]); */
+  /*   const [inputValue, setInputValue] = useState(''); */
+  const [formData, setFormData] = useState({});
 
+  /*   const changeHandler = (event) => {
+    setInputValue(event.target.value);
+  }; */
+
+  const changeFormHandler = (event) => {
+    const { name, value } = event.target;
+
+    setFormData((prevState) => ({ ...prevState, [name]: value }));
+    console.log(formData);
+  };
+
+  /*  const clickIncHandler = () => {
+    setCounter(counter + 1);
+  };
+  const clickDecHandler = () => {
+    setCounter(counter - 1);
+  }; 
+ */
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Header />
+      <Form changeHandler={changeFormHandler} />
+      <View {...formData} />
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
